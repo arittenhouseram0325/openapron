b648a0e9fc94a4f1eefd6cd8d702b597858261ee
arittenhouseram0325 <arittenhouseram0325@live.sunyjefferson.edu>
2026-04-29 14:09:13 -0500

    Update login.php

--- Group Project/Recipe System/login.php
+++ Group Project/Recipe System/login.php
@@ -17,6 +17,7 @@
         if (password_verify($password, $user['password'])) {
             
             //Store user info
+            $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
 
             echo "Login Successful!";
